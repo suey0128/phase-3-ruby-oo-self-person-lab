0309600aeb6710bc7394a55753a934b6916b5b42
suey0128 <syui0128@gmail.com>
2021-07-05 20:42:57 -0700

    commit tree 18b75ffbcaa6cb81972e3b5315703a0f9bbe5f5b

--- lib/person.rb
+++ lib/person.rb
@@ -1,1 +1,88 @@
-# your code goes here
+require 'pry'
+
+class Person
+    attr_reader :name
+    attr_accessor :bank_account, :happiness, :hygiene
+
+
+    def initialize name, bank_account=25, happiness=8, hygiene=8
+        @name = name
+        @bank_account = bank_account
+        @happiness = happiness
+        @hygiene = hygiene
+
+    end
+
+    # def name=(name)
+    #     super(@name)
+    # end
+
+
+#  binding.pry
+    def happiness=(happiness)
+        if happiness < 0
+            @happiness = 0
+        elsif happiness > 10
+            @happiness = 10
+        else 
+            @happiness = happiness
+        end
+    end
+
+    def hygiene=(hygiene)
+        if hygiene < 0
+            @hygiene = 0
+        elsif hygiene > 10
+            @hygiene = 10
+        else 
+            @hygiene = hygiene
+        end
+    end
+
+    def happy? 
+        happiness > 7 ? true : false 
+    end
+
+    def clean? 
+        hygiene > 7 ? true : false 
+    end
+
+    def get_paid num
+        @bank_account += num
+        'all about the benjamins'
+    end
+
+    def take_bath 
+        self.hygiene = @hygiene + 4
+        '♪ Rub-a-dub just relaxing in the tub ♫'
+    end
+
+    def work_out
+        self.hygiene = @hygiene - 3
+        self.happiness = @happiness + 2
+        '♪ another one bites the dust ♫'
+    end
+
+    def call_friend callee
+        self.happiness = @happiness + 3
+        callee.happiness = callee.happiness + 3
+        "Hi #{callee.name}! It's #{self.name}. How are you?"
+    end
+
+    def start_conversation friend, topic
+        if topic == "politics"
+            self.happiness = @happiness - 2
+            friend.happiness = friend.happiness - 2
+            'blah blah partisan blah lobbyist'
+        elsif topic == "weather"
+            self.happiness = @happiness + 1
+            friend.happiness = friend.happiness + 1
+            'blah blah sun blah rain'
+        else 
+            'blah blah blah blah blah'
+        end
+    end
+
+end
+
+# binding.pry
